9ea54072d9bc436308063e8ecfcad9a357a0a099
Paolo224 <picariellopaolo224@gmail.com>
2022-11-18 15:40:10 +0100

    Prima bozza dei dati js

---
+++ Dati.js
@@ -0,0 +1,9 @@
+const name = prompt ('Ciao! Come ti chiami? ')
+const last_name = prompt ('Ciao! Qual è il tuo Cognome? ')
+const color = prompt ('Ciao! Qual è il tuo colore preferito? ')
+
+let dati_completi = name + last_name + color;
+
+console.log(dati_completi)
+
+document.getElementById('dati-utente').innerHTML = dati_completi 
