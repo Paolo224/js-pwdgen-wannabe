15f8f7f17b0cdb553de0909ce393a2a658c22b06
Paolo224 <picariellopaolo224@gmail.com>
2022-11-18 15:41:47 +0100

    aggiunta degli spazzi tra le parole

--- Dati.js
+++ Dati.js
@@ -2,7 +2,7 @@ const name = prompt ('Ciao! Come ti chiami? ')
 const last_name = prompt ('Ciao! Qual è il tuo Cognome? ')
 const color = prompt ('Ciao! Qual è il tuo colore preferito? ')
 
-let dati_completi = name + last_name + color;
+let dati_completi = name + " " + last_name + " " + color;
 
 console.log(dati_completi)
 
